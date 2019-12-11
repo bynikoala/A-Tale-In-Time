e9a90a7883416cfc39ffb68cebcab5bb8b92f78e
Sanngrithr <tomislav.sever@gmx.de>
2019-12-11 12:03:23 +0100

    overlay resize implemented

---
+++ Code/Assets/Scripts/OverlayResizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlayResizer : MonoBehaviour
+{
+    private Rect overlayRect;
+    public Canvas parentCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        overlayRect = this.GetComponent<RectTransform>().rect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect currentsize = parentCanvas.gameObject.GetComponent<RectTransform>().rect;
+
+        overlayRect.height = currentsize.height;
+        overlayRect.width = currentsize.width;
+
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(overlayRect.width, overlayRect.height);
+    }
+}
