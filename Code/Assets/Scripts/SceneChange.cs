a8c4b553c40a63bab6126d773935c30c2594bf8d
Sanngrithr <tomislav.sever@gmx.de>
2019-12-18 21:59:00 +0100

    scenechange script

---
+++ Code/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(SceneManager.GetActiveScene().name == "PrototypePastScene")
+        {
+            SceneManager.LoadScene("PrototypeFutureScene", LoadSceneMode.Single);
+        }
+        else if(SceneManager.GetActiveScene().name == "PrototypeFutureScene")
+        {
+            SceneManager.LoadScene("PrototypePastScene",LoadSceneMode.Single);
+        }
+    }
+}
