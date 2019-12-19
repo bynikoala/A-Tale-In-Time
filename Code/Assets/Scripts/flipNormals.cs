d687de5952a623712020308f553509e1dc50b382
MarKs <markus.schuermann@hs-furtwangen.de>
2019-12-19 15:04:53 +0100

    fixed shoe normal bug via script

---
+++ Code/Assets/Scripts/flipNormals.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class flipNormals : MonoBehaviour
+{
+
+    public Mesh mesh;
+/*
+    Color color;
+
+    [MenuItem("Window/FlipNormals")]
+    public static void ShowWindow()
+    {
+        GetWindow<flipNormals>("FlipNormals");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("flipNormal", EditorStyles.boldLabel);
+
+        mesh = (Mesh)EditorGUILayout.ObjectField(mesh, typeof(Mesh), true);
+
+        if (GUILayout.Button("flipNormal"))
+        {
+            FlipNormals();
+        }
+
+    }
+
+*/
+    void Start()
+    {
+        Mesh mesh = this.gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+
+        Vector3[] normals = mesh.normals;
+        for(int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = -1 * normals[i];
+        }
+        mesh.normals = normals;
+
+
+        for(int i = 0; i < mesh.subMeshCount; i++)
+        {
+            int[] tris = mesh.GetTriangles(i);
+            for(int j = 0; j<tris.Length; j += 3)
+            {
+                int temp = tris[j];
+                tris[j] = tris[j + 1];
+                tris[j + 1] = temp;
+            }
+            mesh.SetTriangles(tris, i);
+        }
+    }
+}
